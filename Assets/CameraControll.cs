43da9f62210231fbe859a512cda47c302014e938
cmxYZ <mikhail.shanygin@urfu.me>
2022-05-18 12:47:28 +0600

    m

---
+++ Assets/CameraControll.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    public enum Direction
+    { 
+        Horizontal,
+        Vertical,
+        None
+    }
+
+    public Transform cameraParent;
+    public Camera mainCamera;
+    public float sensitivity = 0.005f;
+    public float rotateSensitivity = 0.05f;
+    public float zoomSensitivity = 0.05f;
+    public float zoomMin = 40;
+    public float zoomMax = 90;
+
+    private bool touched;
+    private Vector3 startPos;
+    private Direction camDirection = Direction.None;
+    private Vector3 delta;
+    private float zoomPrevMagnitude;
+    private bool firstZoomTouch;
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            startPos = Input.mousePosition;
+            touched = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            touched = false;
+            camDirection = Direction.None;
+        }
+        if (Input.touchCount == 2)
+            ZoomCamera();
+        else 
+        {
+            firstZoomTouch = true;
+            if (touched) MoveCamera();
+        }
+        
+    }
+
+    public void MoveCamera()
+    {
+        delta = startPos - Input.mousePosition;
+        startPos = Input.mousePosition;
+        if (camDirection == Direction.Horizontal || camDirection == Direction.None && Mathf.Abs(delta.x) >= Mathf.Abs(delta.y) && Mathf.Abs(delta.x) > 1.0f)
+        {
+            camDirection = Direction.Horizontal;
+            cameraParent.transform.Rotate(0, -delta.x * rotateSensitivity, 0);
+        }
+        else if (camDirection == Direction.Vertical || camDirection == Direction.None && Mathf.Abs(delta.x) < Mathf.Abs(delta.y) && Mathf.Abs(delta.y) > 1.0f)
+        {
+            camDirection = Direction.Vertical;
+            transform.position = new Vector3(transform.position.x, transform.position.y + delta.y * sensitivity, transform.position.z);
+        }
+    }
+
+    public void ZoomCamera()
+    {
+        Touch touch1 = Input.GetTouch(0);
+        Touch touch2 = Input.GetTouch(1);
+
+        if (firstZoomTouch)
+        {
+            zoomPrevMagnitude = (touch1.position - touch2.position).magnitude;
+            firstZoomTouch = false;
+        }
+
+        float currentMagnitude = (touch1.position - touch2.position).magnitude;
+        float delta = currentMagnitude - zoomPrevMagnitude;
+        zoomPrevMagnitude = currentMagnitude;
+
+        mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView - delta * zoomSensitivity, zoomMin, zoomMax);
+    }
+}
